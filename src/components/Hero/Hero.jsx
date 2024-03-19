9a27fe6a556783dd39082ef4a445f0c2e45e64ee
UjfaA <alexujfa@outlook.com>
2024-03-19 15:19:24 +0100

    added about

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -9,7 +9,9 @@ export const Hero = () => {
 		<div className={styles.content}>
 			<h1 className={styles.title}>Hi, I'm Alex</h1>
 			<p className={styles.description}>
-				I'm a software developer.
+				I am a software developer. My expertise primarily lies in developing robust backend systems.
+				I am also open to engaging with frontend and infrastructure aspects of product development.
+				Regardless of the area, my goal is to craft solutions that are both elegant and comprehensible and <i>just make sense</i>.
 			</p>
 			<a href="mailto:alexujfa@outlook.com"
 				className={styles.contactBtn}>
